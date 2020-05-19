d82108330319c7781e2c5f194aef48e9ee16eaf1
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-05-18 18:41:33 -0600

    Por el momento solo se ha puesto lo que Nelson habia trabajado!

--- Tarea3/tarea_3_00162317.asm
+++ Tarea3/tarea_3_00162317.asm
@@ -12,6 +12,49 @@ section .text
 
 ;----------------------------------------------------------
 ;----------------------------------------------------------
+texto:	mov 	ah, 00h
+	    mov	    al, 03h ; 80 columnas por 25 filas
+        int 	10h
+        call 	texto1	
+        ret
+
+cursor: mov	ah, 01h
+        mov 	ch, 00000000b
+        mov 	cl, 00001110b
+            ;   IRGB
+        int 	10h
+        call 	cursor1
+        ret
+
+w_char:	mov 	ah, 09h
+        mov 	al, cl
+        mov 	bh, 0h
+        mov 	bl, 00001111b
+        mov 	cx, 1h
+        int 	10h
+        ret
+
+kbwait: mov 	ax, 0000h
+        int 	16h
+        ret
+
+m_cursr:mov 	ah, 02h
+        mov 	dx, di  ; columna
+        mov 	dh, 0d ; fila
+        mov 	bh, 0h
+        int 	10h
+        ret
+
+phrase:	mov 	di, 29d
+lupi:	mov 	cl, [msg+di-29d]
+        call    m_cursr
+        call 	w_char
+        inc	di
+        cmp 	di, len
+        jb	    lupi
+        call 	phrase1
+        ret
+;----------------------------------------------------------
 section .data
         msg	    db  "Roberto Ernesto"
         len 	    equ	$-msg+29d
