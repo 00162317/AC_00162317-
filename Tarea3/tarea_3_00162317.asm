88d89711cab437d26e9f69838644d3e2c7da3489
Roberto Hernandez <hernandez.roberto014@gmail.com>
2020-05-18 18:45:48 -0600

    Para crear la fila 2 o el parrafo 2, solamente volvi a reutilizar el codigo que ya estaba creado a diferencia que las llamadas de las funciones las hago en "phrase", "cursor" y "texto"

--- Tarea3/tarea_3_00162317.asm
+++ Tarea3/tarea_3_00162317.asm
@@ -94,6 +94,45 @@ lupi1:	mov 	cl, [msg1+di-1d]
         jb	lupi1
         call    phrase2
         ret        
+
+;----------------------------------------------------------
+
+texto2:	mov 	ah, 00h
+	mov	 al, 03h ; 80 columnas por 25 filas
+        int 	10h
+        ret
+
+cursor2: mov	ah, 01h
+        mov 	ch, 00000000b
+        mov 	cl, 00001110b
+            ;   IRGB
+        int 	10h
+        ret
+
+w_char2:	mov 	ah, 09h
+        mov 	al, cl
+        mov 	bh, 0h
+        mov 	bl, 00001111b
+        mov 	cx, 1h
+        int 	10h
+        ret
+
+m_cursr2:mov 	ah, 02h
+        mov 	dx, di  ; columna
+        mov 	dh, 20d ; fila
+        mov 	bh, 0h
+        int 	10h
+        ret
+
+phrase2:	mov 	di, 55d
+lupi2:  	mov 	cl, [msg2+di-55d]
+            call    m_cursr2
+            call 	w_char2
+            inc	    di
+            cmp 	di, len2
+            jb	    lupi2
+            ret
+;----------------------------------------------------------        
 ;----------------------------------------------------------
 section .data
         msg	    db  "Roberto Ernesto"
